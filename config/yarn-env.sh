172a5f7bc26740fd492a36a039946f0b6e743423
pradeepmuntha <pradeepmuntha86@gmail.com>
2016-04-19 16:08:49 +0530

    Add Yarn layer config to containers

---
+++ config/yarn-env.sh
@@ -0,0 +1,54 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# User for YARN daemons
+export HADOOP_YARN_USER=${HADOOP_YARN_USER:-hdfs}
+
+# resolve links - $0 may be a softlink
+export YARN_CONF_DIR="${YARN_CONF_DIR:-"/usr/local/hadoop"}"
+
+# some Java parameters
+#JAVA_HOME will be configured by the ClusterConsole config generator.
+JAVA_HEAP_MAX=-Xmx1000m 
+
+# check envvars which might override default args
+if [ "$YARN_HEAPSIZE" != "" ]; then
+  #echo "run with heapsize $YARN_HEAPSIZE"
+  JAVA_HEAP_MAX="-Xmx""$YARN_HEAPSIZE""m"
+  #echo $JAVA_HEAP_MAX
+fi
+
+# so that filenames w/ spaces are handled correctly in loops below
+IFS=
+
+YARN_LOG_DIR="/usr/local/hadoop/logs"
+# default log directory & file
+if [ "$YARN_LOG_DIR" = "" ]; then
+  YARN_LOG_DIR="$YARN_HOME/logs"
+fi
+if [ "$YARN_LOGFILE" = "" ]; then
+  YARN_LOGFILE='yarn.log'
+fi
+
+# default policy file for service-level authorization
+if [ "$YARN_POLICYFILE" = "" ]; then
+  YARN_POLICYFILE="hadoop-policy.xml"
+fi
+
+# restore ordinary behaviour
+unset IFS
+
+export JAVA_HOME="/usr/local/jdk"
+export JAVA=$JAVA_HOME
--- startup.sh
+++ startup.sh
@@ -12,7 +12,7 @@ fi
 
 case $ACT in
 
-fresh_start)
+start)
 	if [ -z $VOL ] || [ ! -d $VOL ]
 	then
 		echo "Volume Directory not mentioned as fist argument or directory does not exist. Please ensure pre-requisite commands mentioned in Readme section of GIT are executed as root user"
@@ -22,37 +22,46 @@ fresh_start)
 	docker pull pradeepmuntha/hadoop_centos_autobuild:latest
 
 #Master Node Commands
-	docker run -tdi --rm --user hdfs --name master --hostname master1 -p 50070:50070 --add-host=master1:172.17.0.2 --add-host=slave2:172.17.0.4 --add-host=slave1:172.17.0.3 -v $VOL/master/data:/data -v $VOL/logs:$HADOOP_PREFIX/logs hadoop:mod
+	docker run -tdi  --user hdfs --name master --hostname master1 -p 50070:50070 --add-host=master1:172.17.0.2 --add-host=slave2:172.17.0.4 --add-host=slave1:172.17.0.3 -v $VOL/master/data:/data -v $VOL/logs:$HADOOP_PREFIX/logs hadoop:mod
 	docker exec -u hdfs master $HADOOP_PREFIX/bin/hdfs namenode -format
 	docker exec -u hdfs master $HADOOP_PREFIX/sbin/hadoop-daemon.sh start namenode
 
 #Slave Containers Creation
-	docker run -tdi --rm --user hdfs --name slave1 --hostname slave1 --add-host=master1:172.17.0.2 --add-host=slave2:172.17.0.4 --add-host=slave1:172.17.0.3 -v $VOL/slave1/data-1:/data-1 -v $VOL/slave1/data-2:/data-2 -v $VOL/logs:$HADOOP_PREFIX/logs hadoop:mod
-	docker run -tdi --rm --user hdfs --name slave2 --hostname slave2 --add-host=master1:172.17.0.2 --add-host=slave2:172.17.0.4 --add-host=slave1:172.17.0.3 -v $VOL/slave2/data-1:/data-1 -v $VOL/slave2/data-2:/data-2 -v $VOL/logs:$HADOOP_PREFIX/logs hadoop:mod
+	docker run -tdi  --user hdfs --name slave1 --hostname slave1 --add-host=master1:172.17.0.2 --add-host=slave2:172.17.0.4 --add-host=slave1:172.17.0.3 -v $VOL/slave1/data-1:/data-1 -v $VOL/slave1/data-2:/data-2 -v $VOL/logs:$HADOOP_PREFIX/logs hadoop:mod
+	docker run -tdi  --user hdfs --name slave2 --hostname slave2 --add-host=master1:172.17.0.2 --add-host=slave2:172.17.0.4 --add-host=slave1:172.17.0.3 -v $VOL/slave2/data-1:/data-1 -v $VOL/slave2/data-2:/data-2 -v $VOL/logs:$HADOOP_PREFIX/logs hadoop:mod
 
 #Slave perm fix and service startup
 	docker exec -u hdfs slave1 mkdir /data-{1,2}/hdfs
 	docker exec -u hdfs slave2 mkdir /data-{1,2}/hdfs
 	docker exec -u hdfs slave1 $HADOOP_PREFIX/sbin/hadoop-daemon.sh start datanode
 	docker exec -u hdfs slave2 $HADOOP_PREFIX/sbin/hadoop-daemon.sh start datanode
+#Yarn layer startup
+	docker exec -u hdfs master $HADOOP_PREFIX/sbin/yarn-daemon.sh start resourcemanager
+	docker exec -u hdfs master $HADOOP_PREFIX/sbin/yarn-daemon.sh start nodemanager
 	;;
 
 start_services)
 	docker exec -u hdfs master $HADOOP_PREFIX/sbin/hadoop-daemon.sh start namenode
 	docker exec -u hdfs slave1 $HADOOP_PREFIX/sbin/hadoop-daemon.sh start datanode
         docker exec -u hdfs slave2 $HADOOP_PREFIX/sbin/hadoop-daemon.sh start datanode
+	docker exec -u hdfs master $HADOOP_PREFIX/sbin/yarn-daemon.sh start resourcemanager
+	docker exec -u hdfs master $HADOOP_PREFIX/sbin/yarn-daemon.sh start nodemanager
 	;;
 
 stop_services)
 	docker exec -u hdfs slave2 $HADOOP_PREFIX/sbin/hadoop-daemon.sh stop datanode
 	docker exec -u hdfs slave1 $HADOOP_PREFIX/sbin/hadoop-daemon.sh stop datanode
 	docker exec -u hdfs master $HADOOP_PREFIX/sbin/hadoop-daemon.sh stop namenode
+	docker exec -u hdfs master $HADOOP_PREFIX/sbin/yarn-daemon.sh stop nodemanager
+	docker exec -u hdfs master $HADOOP_PREFIX/sbin/yarn-daemon.sh stop resourcemanager
 	;;
 
 stop)
 	docker exec -u hdfs slave2 $HADOOP_PREFIX/sbin/hadoop-daemon.sh stop datanode
         docker exec -u hdfs slave1 $HADOOP_PREFIX/sbin/hadoop-daemon.sh stop datanode
         docker exec -u hdfs master $HADOOP_PREFIX/sbin/hadoop-daemon.sh stop namenode
+	docker exec -u hdfs master $HADOOP_PREFIX/sbin/yarn-daemon.sh stop nodemanager
+	docker exec -u hdfs master $HADOOP_PREFIX/sbin/yarn-daemon.sh stop resourcemanager
 	docker stop master slave1 slave2 
 	docker rm master slave1 slave2
 	;;
