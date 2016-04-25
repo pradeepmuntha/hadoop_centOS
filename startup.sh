e2c1633ee90d4a9b06977333961364c05cf4a5b2
pradeepmuntha <pradeepmuntha86@gmail.com>
2016-04-25 19:16:07 +0530

    Creation of custom bridge network to specify fixed IP add. for individual containers

--- startup.sh
+++ startup.sh
@@ -10,6 +10,7 @@ then
 	exit 1
 fi
 
+
 case $ACT in
 
 start)
@@ -21,14 +22,24 @@ start)
 
 	docker pull pradeepmuntha/hadoop_centos_autobuild:latest
 
+	CNT=`docker network ls | grep "custom_bridge" | grep -v grep |wc -l`
+
+	if [ "$CNT" -ne 0 ]
+	then
+        	echo "Docket network \"custom_bridge\" network already exists on the system so not re-creating it !!!!. Execute 'docker network ls' command to check list of docker networks on host" 
+	else
+        	docker network create -d bridge --label custom_bridge --subnet=172.98.0.1/24 --gateway 172.98.0.1 --opt "com.docker.network.bridge.enable_ip_masquerade"="true" --opt "com.docker.network.bridge.enable_icc"="true" --opt "com.docker.network.bridge.host_binding_ipv4"="0.0.0.0" --opt "com.docker.network.mtu"="1500" --opt "com.docker.network.bridge.name"="docker-custom-bridge-0" custom_bridge
+
+	fi
+
 #Master Node Commands
-	docker run -tdi  --user hdfs --name master --hostname master1 -p 50070:50070 --add-host=master1:172.17.0.2 --add-host=slave2:172.17.0.4 --add-host=slave1:172.17.0.3 -v $VOL/master/data:/data -v $VOL/logs:$HADOOP_PREFIX/logs hadoop:mod
+	docker run -tdi  --user hdfs --name master --hostname master1 --net custom_bridge --ip 172.98.0.2 -p 50070:50070 -p 8088:8088 --add-host=master1:172.98.0.2 --add-host=slave2:172.98.0.4 --add-host=slave1:172.98.0.3 -v $VOL/master/data:/data -v $VOL/logs:$HADOOP_PREFIX/logs hadoop:mod
 	docker exec -u hdfs master $HADOOP_PREFIX/bin/hdfs namenode -format
 	docker exec -u hdfs master $HADOOP_PREFIX/sbin/hadoop-daemon.sh start namenode
 
 #Slave Containers Creation
-	docker run -tdi  --user hdfs --name slave1 --hostname slave1 --add-host=master1:172.17.0.2 --add-host=slave2:172.17.0.4 --add-host=slave1:172.17.0.3 -v $VOL/slave1/data-1:/data-1 -v $VOL/slave1/data-2:/data-2 -v $VOL/logs:$HADOOP_PREFIX/logs hadoop:mod
-	docker run -tdi  --user hdfs --name slave2 --hostname slave2 --add-host=master1:172.17.0.2 --add-host=slave2:172.17.0.4 --add-host=slave1:172.17.0.3 -v $VOL/slave2/data-1:/data-1 -v $VOL/slave2/data-2:/data-2 -v $VOL/logs:$HADOOP_PREFIX/logs hadoop:mod
+	docker run -tdi  --user hdfs --name slave1 --hostname slave1 --net custom_bridge --ip 172.98.0.3 --add-host=master1:172.98.0.2 --add-host=slave2:172.98.0.4 --add-host=slave1:172.98.0.3 -v $VOL/slave1/data-1:/data-1 -v $VOL/slave1/data-2:/data-2 -v $VOL/logs:$HADOOP_PREFIX/logs hadoop:mod
+	docker run -tdi  --user hdfs --name slave2 --hostname slave2  --net custom_bridge --ip 172.98.0.4 --add-host=master1:172.98.0.2 --add-host=slave2:172.98.0.4 --add-host=slave1:172.98.0.3 -v $VOL/slave2/data-1:/data-1 -v $VOL/slave2/data-2:/data-2 -v $VOL/logs:$HADOOP_PREFIX/logs hadoop:mod
 
 #Slave perm fix and service startup
 	docker exec -u hdfs slave1 mkdir /data-{1,2}/hdfs
@@ -74,6 +85,6 @@ destroy)
 	fi
 	;;
 *)	
-	echo "Wrong command usage. Supported commands are start, stop, start_services, stop_services and destroy"
+	echo "Wrong command usage. Supported commands are start, stop and destroy"
 	;;
 esac
